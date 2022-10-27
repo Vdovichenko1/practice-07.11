577667d4c3421dee3e09be11436ba04b86daf07d
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-10-27 20:00:56 +0300

    add task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
 // import Accordion from "./Accordion/Accordion";
-// import FAQ from "./FAQ/FAQ";
+import FAQ from "./FAQ/FAQ";
 // import Gallery from "./Gallery/Gallery";
-import Expresso from "./Expresso/Expresso";
 // import Products from "./Products/Products";
 
 export const App = () => {
   return (
-    <div>
+    <>
       {/* <Accordion /> */}
-      {/* <FAQ/> */}
+      <FAQ/>
       {/* <Gallery/> */}
       {/* <Products/> */}
-      <Expresso/>
-    </div>
+
+    </>
   );
 };
--- src/components/Expresso/Expresso.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-    margin:0 auto;
-    padding-top: 100px;
-
-`
