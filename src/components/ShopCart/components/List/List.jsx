bea5166239df1140a9414c2df6a770f022b5ddc9
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-10-31 17:26:48 +0200

    render items from backend

--- src/components/ShopCart/components/List/List.jsx
+++ src/components/ShopCart/components/List/List.jsx
@@ -1,18 +1,16 @@
-import { Button} from 'react-bootstrap';
-import {ListCart, ListItem} from './List.styled'
+import { Button,Card } from 'react-bootstrap';
+import { ListCart, ListItem } from './List.styled';
 const List = ({ items }) => (
   <ListCart>
-    {items.map(({ name, id, description, price }) => (
-        <ListItem key={id}>
-          <ListItem.Header>{name}</ListItem.Header>
-          <ListItem.Body>
-            <ListItem.Title>{price}</ListItem.Title>
-            <ListItem.Text>{description}
-            </ListItem.Text>
-            <Button variant="primary">Delete</Button>
-          </ListItem.Body>
-        </ListItem>
-
+    {items.map(({ name, description, price }) => (
+      <ListItem key={name}>
+        <Card.Header>{name}</Card.Header>
+        <Card.Body>
+          <Card.Title>{price}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary">Delete</Button>
+        </Card.Body>
+      </ListItem>
     ))}
   </ListCart>
 );
--- src/components/ShopCart/components/List/List.styled.jsx
+++ src/components/ShopCart/components/List/List.styled.jsx
@@ -1,12 +1,13 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
-import {  Card, ListGroup } from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 
 export const ListCart = styled(ListGroup)`
-flex-direction: row;
-flex-wrap: wrap;
-
-`
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 40px 20px;
+  gap: 20px;
+`;
 export const ListItem = styled(Card)`
-    flex-basis: calc(100% / 3);
-`
+  flex-basis: calc((100% - 40px) / 3);
+`;
